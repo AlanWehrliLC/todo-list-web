647fcdfc2199b61d67149cc07daa1d9e284a72f8
AlanWehrliLC <wehrlialan1@gmail.com>
2022-11-02 18:05:06 -0300

    Showing how many tasks have been created, and showing how many remain to be done

--- src/components/TaskCounter/index.tsx
+++ src/components/TaskCounter/index.tsx
@@ -1,10 +1,15 @@
+import { todoGetAll } from "../../storage/todo/todoGetAll"
 import "./styles.scss"
 
 export function TaskCounter(){
+    const tasks = todoGetAll()
+    const numberOfTasks = tasks.length
+    const tasksDone = tasks.filter(todo => todo.done == true).length
+
     return (
         <div id="taskCounter">
-            <div className="created">Created tasks<span>0</span></div>
-            <div className="completed">Completed<span>0</span></div>
+            <div className="created">Created tasks<span>{numberOfTasks}</span></div>
+            <div className="completed">Completed<span>{tasksDone} out of {numberOfTasks}</span></div>
         </div>
     )
 }
